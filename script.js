24784bb6829043e131dc99c6e051f3aea9d6d6da
Agus Hendra <agushendra29@gmail.com>
2021-12-16 11:34:21 +0700

    fixing origin

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleProfil() {
+  document.getElementById("dropdown-profil").classList.toggle("show");
+}
+function toggleAkademik() {
+  document.getElementById("dropdown-akademik").classList.toggle("show");
+}
+
+// window.onclick = function (event) {
+//   if (!event.target.matches(".dropbtn")) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains("show")) {
+//         openDropdown.classList.remove("show");
+//       }
+//     }
+//   }
+// };
